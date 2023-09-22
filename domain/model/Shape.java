9340ddecf335688220456f18b18dc9ec0e2f937f
JuanCarlosGA <juan.giraldo43633@ucaldas.edu.co>
2023-09-22 08:08:02 -0500

    Update to bugs on the last push

--- domain/model/Shape.java
+++ domain/model/Shape.java
@@ -1,14 +1,36 @@
 package domain.model;
 
 public class Shape{
-    private int x;
-    private int y;
+    private int x = 0;
+    private int y = 0;
 
-    Shape() {
+    public Shape() {
 
     }
 
-    public int calculateArea(){
-        return this.x*this.y;
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Make setters and getters for x and y, in the setters make that only positive and bellow 1000 values can be set
+    public void setX(int x) {
+        if (x >= 0 && x < 1000) {
+            this.x = x;
+        }
+    }
+    
+    public void setY(int y) {
+        if (y >= 0 && y < 1000) {
+            this.y = y;
+        }
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
--- ui/Test.java
+++ ui/Test.java
@@ -1,5 +1,23 @@
 package ui;
+import domain.model.Shape;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        // make two var of Shape called shape1 and shape2 and give them different values
+        Shape shape1 = new Shape(1, 2);
+        Shape shape2 = new Shape(3, 4);
+
+        // print the values of shape1 and shape2
+        System.out.println("shape1: " + shape1.getX() + ", " + shape1.getY());
+        System.out.println("shape2: " + shape2.getX() + ", " + shape2.getY());
+
+        // use setX and setY to change the values of shape1 and shape2 and print them again
+        shape1.setX(5);
+        shape1.setY(6);
+        shape2.setX(7);
+        shape2.setY(8);
+
+        System.out.println("shape1: " + shape1.getX() + ", " + shape1.getY());
+        System.out.println("shape2: " + shape2.getX() + ", " + shape2.getY());
+    }
 }
